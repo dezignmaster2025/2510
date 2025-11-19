6dc8bbfdcd2e38386efbd08ae8e1bb037626a092
dezignmaster2025 <ghudgin9089@student.gwinnetttech.edu>
2025-11-19 17:06:23 -0500

    Lesson 13: Initial commit for quote-app project

---
+++ Lessons/lesson13/quote-app/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function QuoteBox() {
+  return (
+    <div className="quote-box">
+      <p className="quote-text">
+        "The only limit to our realization of tomorrow is our doubts of today."
+      </p>
+      <p className="quote-author">- Franklin D. Roosevelt</p>
+    </div>
+  );
+}
+
+export default QuoteBox;
