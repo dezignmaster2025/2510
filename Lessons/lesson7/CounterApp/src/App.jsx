d8d91253eb25721a8ca0a2c3e009edf683c1d417
dezignmaster2025 <ghudgin9089@student.gwinnetttech.edu>
2025-12-03 14:24:24 -0500

    Lesson 7 counter app

--- Lessons/lesson7/CounterApp/src/App.jsx
+++ Lessons/lesson7/CounterApp/src/App.jsx
@@ -5,22 +5,23 @@ function App() {
   return (
     <>
       <h2>Welcome to the Counter App</h2>
-      {/* Pass the title prop to Counter */}
+      {
+
+      }
       <Counter title="Simple Counter" />
     </>
   );
 }
 
 function Counter({ title }) {
-  // State for the counter
+  
   const [count, setCount] = useState(0);
 
-  // Handler to increase count
+  
   const handleIncrease = () => {
     setCount(count + 1);
   };
-
-  // Handler to reset count
+  
   const handleReset = () => {
     setCount(0);
   };
